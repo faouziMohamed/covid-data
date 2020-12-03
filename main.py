f257a297141daf7c3e60e3fcb2213ccef2e0d83c
Faouzi <ttyuid@protonmail.com>
2020-12-03 05:15:28 +0100

    New design and reading style (qss file) for applying style to every combobox

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys
 
+from PyQt5.QtCore import QFile, QIODevice, QTextStream
 from PyQt5.QtWidgets import QApplication
 
 from src.dataView import CovidView
-
+# noinspection PyUnresolvedReferences
+import src.ressources_rc
 
 if __name__ == '__main__':
+    style_file = QFile(':/style.qss')
+    style_file.open(QIODevice.ReadOnly)
+    sheet = QTextStream(style_file)
+    style_sheet = sheet.readAll()
     app = QApplication(sys.argv)
+    app.setStyleSheet(style_sheet)
     view = CovidView()
     view.show()
     sys.exit(app.exec_())
