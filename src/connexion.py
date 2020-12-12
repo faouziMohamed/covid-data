20e53aca8d03e1643fd768d869549e2f240c5dbf
Faouzi <ttyuid@protonmail.com>
2020-12-12 01:23:35 +0100

    New API to retrieve data in the database

--- src/connexion.py
+++ src/connexion.py
@@ -67,23 +67,24 @@ class DbConnexion:
         with self._engine.connect() as con:
             con.execute("""
                 CREATE VIEW model_view AS
-                SELECT dates, continent, location, new_tests, new_cases,
+                SELECT date, continent, location, new_tests, new_cases,
                        new_deaths,total_tests, total_cases, total_deaths
                 FROM continents, countries, cases
                 WHERE cases.idl = countries.idl AND 
                       countries.idc = continents.idc
                 ORDER BY 
-                    dates DESC , 
+                    date DESC , 
                     total_cases DESC, 
                     total_deaths DESC;
                 """)
             model_view = self.get_table('model_view', MetaData())
             select_query = select([model_view])
+            today, yesterday = utils.today(), utils.yesterday()
             select_query = select_query.where(
-                or_(model_view.columns.dates == utils.today(),
-                    model_view.columns.dates == utils.yesterday())
+                or_(model_view.columns.date == today,
+                    model_view.columns.date == yesterday)
             )
-            self._view = pd.read_sql_query(select_query, con=self._engine)
+        self._view = pd.read_sql_query(select_query, con=self._engine)
         return self.view
 
     def __insert_data_for_first_time(self):
@@ -139,8 +140,8 @@ class DbConnexion:
                    'total_tests', 'total_cases', 'total_deaths']
         cases = self._df.loc[:, columns]
         cases = cases.replace(list(countries.location), countries.index)
-        cases = cases.rename(columns={'location': 'idl', 'date': 'dates'})
-        cases = cases.sort_values(['dates', 'total_cases', 'total_deaths'],
+        cases = cases.rename(columns={'location': 'idl'})
+        cases = cases.sort_values(['date', 'total_cases', 'total_deaths'],
                                   ascending=False)
         cases = cases.reset_index(drop=True)
         cases.index.name = 'id'
@@ -156,16 +157,24 @@ class DbConnexion:
         query = (continents_df.continent == continent)
         return int(continents_df.loc[query].index[0])
 
-    def filter_data_by_date(self, a_date: str) -> pd.DataFrame:
-        model_view = self.get_table('model_view', MetaData())
-        query = select([model_view]).where(model_view.columns.dates == a_date)
-        self._view = pd.read_sql_query(query, con=self._engine)
-        return self._view
+    def filter_data_by(self, by: str, rows_pattern: str) -> pd.DataFrame:
+        """
+        Get data in the view 'model_view' filtered in the column `by`
+        where rows are equal to `rows_pattern`. This will make a select query
+        with these parameter.
 
-    def filter_data_by_continent(self, a_continent) -> pd.DataFrame:
+        Example: Filter in the column Country and the country to get results is
+        'Comoros'. so by = 'country' and rows_pattern = 'Comoros'
+        and this perform the select query :
+        SELECT 'country' FROM model_view WHERE country = 'Comoros'
+
+        :param by: column in the database
+        :param rows_pattern: The pattern to search in the database
+        :return: Pandas.DataFrame Table
+        """
         model_view = self.get_table('model_view', MetaData())
         select_query = select([model_view])
-        select_query.where(model_view.columns.continent == a_continent)
+        select_query.where(model_view.columns[by] == rows_pattern)
         self._view = pd.read_sql_query(select_query, con=self._engine)
         return self._view
 
