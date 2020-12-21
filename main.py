d895b11eabd5e33fe6c34796d772524ed0c04c4b
Faouzi <ttyuid@protonmail.com>
2020-12-21 17:18:54 +0100

    optimize imports

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 
-from PyQt5.QtCore import QFile, QIODevice, QTextStream
-from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtWidgets, QtCore
 
 # noinspection PyUnresolvedReferences
 import src.generated.resources_rc
@@ -9,14 +8,14 @@ from src.dataView import CovidView
 
 
 def read_stylesheet(file_name: str = ':/style.qss') -> str:
-    style_file = QFile(file_name)
-    style_file.open(QIODevice.ReadOnly)
-    sheet = QTextStream(style_file)
+    style_file = QtCore.QFile(file_name)
+    style_file.open(QtCore.QIODevice.ReadOnly)
+    sheet = QtCore.QTextStream(style_file)
     return sheet.readAll()
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     style_sheet = read_stylesheet()
     app.setStyleSheet(style_sheet)
     view = CovidView()
